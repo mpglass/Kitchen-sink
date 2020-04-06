ff11501432ba92d21cad51118502493bcc313f2b
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-04-06 17:55:16 -0500

    fixed age check

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-var name = 'Michelle'
+var myName = 'Michelle'
 const statesOfUsa = '50'
 var sum = (5 + 4)
+
 function sayHello () {
     window.alert("Hello World")
 }
 sayHello();
+
+
+function ageCheck (name, age) { 
+    if (age<21) {
+    window.alert("Sorry" + name + "you aren't old enough to view this page!" )}
+}
+ageCheck ( 
+    'Charles', 
+    21
+);
+ageCheck (
+    'Abby',
+    27
+);
+ageCheck (
+    'James',
+    18
+);
+ageCheck(
+    'John',
+    17);
+
+
+var vegetables = ['broccoli, yellow squash, onions, green beans, carrots, spinach, cauliflower'];
+
+var pet = {
+    name: 'Lady Fluffington',
+    breed: 'Pembroke Welsh Corgi'
+}
+
+console.log(pet)
